de4be547e3c92ae9b4f9b808b6ad601c73abacea
Vladislav Zhuravskiy <Swiglek@gmail.com>
2022-06-02 15:17:18 +0300

    Верстка страницы, убрал google-api

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,23 @@ def create_form():
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', "POST"])
 def main_page():
-    return render_template('main.html')
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        print(len(data))
+        print(type(data))
+        return data
+    elif request.method == "GET":
+        # return render_template('main.html')
+        return render_template('create_form_sheet.html')
 
+
+@app.route('/user/<string:username>')
+def page_user(username):
+    return render_template('index.html', username=username)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- flask_server/static/js/button.js
+++ flask_server/static/js/button.js
@@ -1,20 +1,20 @@
 
 function add_str() {
-    const element = document.getElementById("form");
+    const element = document.getElementById("form_timesheet");
     var element_2 = document.createElement('div')
     var id_form = document.getElementById('form')
 
     element_2.innerHTML = '<div class="form-group">\n' +
         '                    <div class="input-group">\n' +
-        '                        <input type="text" class="form-control" placeholder="Этап">\n' +
-        '                        <input type="text" class="form-control" placeholder="Этаж">\n' +
-        '                        <input type="text" class="form-control" placeholder="Здание">\n' +
+        '                        <input type="text" class="form-control" placeholder="Этап" name="stage">\n' +
+        '                        <input type="text" class="form-control" placeholder="Этаж" name="level">\n' +
+        '                        <input type="text" class="form-control" placeholder="Здание" name="build">\n' +
         '                    </div>\n' +
         '                    <div class="input-group">\n' +
-        '                        <input type="number" class="form-control" placeholder="Деж.П">\n' +
-        '                        <input type="number" class="form-control" placeholder="Деж.Ф">\n' +
-        '                        <input type="number" class="form-control" placeholder="ИТР.П">\n' +
-        '                        <input type="number" class="form-control" placeholder="ИТР.Ф">\n' +
+        '                        <input type="number" class="form-control" placeholder="Деж.П" name="dej_p">\n' +
+        '                        <input type="number" class="form-control" placeholder="Деж.Ф" name="dej_f">\n' +
+        '                        <input type="number" class="form-control" placeholder="ИТР.П" name="itr_p">\n' +
+        '                        <input type="number" class="form-control" placeholder="ИТР.Ф" name="itr_f">\n' +
         '                    </div>\n' +
         '                            <div class="line-separator"></div>' +
         '                </div>'
@@ -22,7 +22,7 @@ function add_str() {
     element.append(element_2)
 }
 
-let base = document.getElementById('form')
+let base = document.getElementById('form_timesheet')
 let but_del = document.getElementById('clear')
 
 but_del.addEventListener('click', ClearFunction)
@@ -32,3 +32,4 @@ function ClearFunction() {
         base.removeChild(base.lastChild)
     }
 }
+
